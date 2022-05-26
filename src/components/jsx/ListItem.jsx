de32cc8b43e1f24644b1317bd750a8d27112e21b
Avishai-Peretz <Avishai651@gmail.com>
2022-05-26 15:02:27 +0300

    cards onClick update v2

--- src/components/jsx/ListItem.jsx
+++ src/components/jsx/ListItem.jsx
@@ -2,31 +2,32 @@ import React, { Component } from 'react'
 
 
 export default class ListItem extends Component {
+  onClickHandler = async () => {
+    const notSameItem =await this.props.selectedItem !== this.props.id ? true : false;
+    const sameItem =await this.props.selectedItem === this.props.id ? true : false;
+    if (notSameItem ) {
+      await this.props.setState({
+        selectedItem: this.props.id,
+        inputValue: { name: await this.props.name, picture: await this.props.picture },
+      })
+      
+    }else if (sameItem) {
+      await this.props.setState({
+        selectedItem: null,
+        inputValue: { name: "", picture: "" },
+      })
+    } else {
+      await this.props.setState({
+        selectedItem: null,
+        inputValue: { name: "", picture: "" },
+      })
+    }
+  }
   render() {
     return (
       <div
         className='todo-item column-c-c'
-        onClick={async () => {
-          const notSameItem =await this.props.selectedItem !== this.props.id ? true : false;
-          const sameItem =await this.props.selectedItem === this.props.id ? true : false;
-          if (notSameItem ) {
-            await this.props.setState({
-              selectedItem: this.props.id,
-              inputValue: { name: await this.props.name, picture: await this.props.picture },
-            })
-            
-          }else if (sameItem) {
-            await this.props.setState({
-              selectedItem: null,
-              inputValue: { name: "", picture: "" },
-            })
-          } else {
-            await this.props.setState({
-              selectedItem: null,
-              inputValue: { name: "", picture: "" },
-            })
-          }
-        }}
+        onClick={this.onClickHandler}
         data-selected={this.props.selectedItem === this.props.id ? true : false}
       >
         {this.props.name}
